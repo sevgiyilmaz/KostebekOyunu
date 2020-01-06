d67fc7b7f17a755fa41b2303f39ceabe02d7ae3c
sevgi yılmaz <ysevgiaslan@gmail.com>
2020-01-06 23:55:08 +0300

    Add project files.

---
+++ KostebekOyunu/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace KostebekOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnBaslaDurdur = new System.Windows.Forms.Button();
+            this.pnlTarla = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblPuan = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnBaslaDurdur
+            // 
+            this.btnBaslaDurdur.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnBaslaDurdur.Location = new System.Drawing.Point(77, 6);
+            this.btnBaslaDurdur.Margin = new System.Windows.Forms.Padding(2);
+            this.btnBaslaDurdur.Name = "btnBaslaDurdur";
+            this.btnBaslaDurdur.Size = new System.Drawing.Size(134, 47);
+            this.btnBaslaDurdur.TabIndex = 7;
+            this.btnBaslaDurdur.Text = "Basla";
+            this.btnBaslaDurdur.UseVisualStyleBackColor = false;
+            this.btnBaslaDurdur.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pnlTarla
+            // 
+            this.pnlTarla.Location = new System.Drawing.Point(7, 61);
+            this.pnlTarla.Margin = new System.Windows.Forms.Padding(2);
+            this.pnlTarla.Name = "pnlTarla";
+            this.pnlTarla.Size = new System.Drawing.Size(560, 565);
+            this.pnlTarla.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblPuan
+            // 
+            this.lblPuan.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblPuan.Location = new System.Drawing.Point(355, 8);
+            this.lblPuan.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPuan.Name = "lblPuan";
+            this.lblPuan.Size = new System.Drawing.Size(141, 45);
+            this.lblPuan.TabIndex = 8;
+            this.lblPuan.Text = "PUAN=0";
+            this.lblPuan.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 631);
+            this.Controls.Add(this.lblPuan);
+            this.Controls.Add(this.btnBaslaDurdur);
+            this.Controls.Add(this.pnlTarla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBaslaDurdur;
+        private System.Windows.Forms.Panel pnlTarla;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblPuan;
+    }
+}
+
---
+++ KostebekOyunu/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KostebekOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random rnd = new Random();
+        int puan = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int x = 0, y = 0, z = 1;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 11; j++)
+                {
+                    Button btn = new Button();
+                    btn.Name = "btn" + z.ToString();
+                    btn.Width = btn.Height = 50;
+                    btn.BackColor = Color.SaddleBrown;
+                    btn.Location = new Point(3 + x, 3 + y);
+                    btn.Click += Btn_Click;
+                    btn.MouseLeave += Btn_MouseLeave;
+                    pnlTarla.Controls.Add(btn);
+                    x += 56;
+                    z++;
+                }
+                y += 56;
+                x = 0;
+            }
+        }
+        private void Btn_MouseLeave(object sender,EventArgs e)
+        {
+            Button ilkRengeDon = (Button)sender;
+            if (ilkRengeDon.BackColor!=Color.Goldenrod)
+            {
+                ilkRengeDon.BackColor = Color.SaddleBrown;
+            }
+        }
+        bool tiklandimi = true;
+        private void Btn_Click(object sender,EventArgs e)
+        {
+            Button tiklanan = (Button)sender;
+            if (tiklanan.BackColor==Color.Goldenrod&&tiklandimi)
+            {
+                lblPuan.Text = "PUAN=   " + (++puan).ToString();
+                tiklanan.BackColor = Color.DarkOliveGreen;
+                tiklandimi = false;
+            }
+            else if (!tiklandimi)
+            {
+                lblPuan.Text = "PUAN=   " + (--puan).ToString();
+            }
+            else if (BaslaDurdur)
+            {
+                lblPuan.Text = "PUAN=   " + (--puan).ToString();
+                tiklanan.BackColor = Color.Maroon;
+            }
+        }
+        int kahverengi = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int kostebek = rnd.Next(0, 101);
+            pnlTarla.Controls[kostebek].BackColor = Color.Goldenrod;
+            pnlTarla.Controls[kahverengi].BackColor = Color.SaddleBrown;
+            kahverengi = kostebek;
+            tiklandimi = true;
+            if (puan<0)
+            {
+                timer1.Interval = 2000;
+            }
+            else if (puan>=0 && puan<25)
+            {
+                timer1.Interval = 1500;
+            }
+            else
+            {
+                timer1.Interval = 1000;
+            }
+        }
+        bool BaslaDurdur = false;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btnBaslaDurdur.Enabled = true;
+            if (BaslaDurdur)
+            {
+                btnBaslaDurdur.Text = "Başla";
+                BaslaDurdur = false;
+            }
+            else
+            {
+                btnBaslaDurdur.Text = "Durdur";
+                BaslaDurdur = true;
+            }
+            timer1.Enabled = !timer1.Enabled;
+            tiklandimi = false;
+        }
+       
+    }
+}
